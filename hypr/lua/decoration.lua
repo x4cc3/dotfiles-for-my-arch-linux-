ebaf9599c1d0355a28e74a9dcacc2e9298e0f6f0
xaccefy <xaccefy@phantom.burn>
2026-07-27 04:43:47 +0800

    set windows rounding to 6px, waybar 24px height with 5px padding

--- hypr/lua/decoration.lua
+++ hypr/lua/decoration.lua
@@ -3,7 +3,7 @@
 ----------------
 hl.config({
     decoration = {
-        rounding = 0,
+        rounding = 6,
         blur = {
             enabled = false,
             size = 8,
@@ -17,5 +17,27 @@ hl.config({
         fullscreen_opacity = 1.0,
     },
     layerrule = {
+    },
+})
+
+----------------
+-- Group (tabbed containers) settings
+----------------
+hl.config({
+    group = {
+        groupbar = {
+            enabled = true,
+            render_titles = true,
+            height = 24,
+            font_size = 16,
+            gradients = true,
+            col = {
+                active = "0xcc3a3a3a",
+                inactive = "0xcc000000",
+                locked_active = "0xcc4a3030",
+                locked_inactive = "0xcc000000",
+            },
+        },
+        insert_after_current = true,
     },
 })
